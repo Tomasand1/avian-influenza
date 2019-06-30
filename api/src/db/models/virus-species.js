53b66c3ceae3ea9e2c85fc47f14431e645646e59
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:30:24 +0100

    Set up virus species model

--- api/src/db/models/virus-species.js
+++ api/src/db/models/virus-species.js
@@ -1,10 +1,29 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
+export default sequelize => {
+    const VirusSpecies = sequelize.define(
+        'VirusSpecies',
+        {
+            virusId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'virus_id',
+            },
+            speciesId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'species_id',
+            },
+        },
+        {
+            tableName: 'virus_species',
+            timestamps: false,
+        },
+    );
 
-export default (sequelize) => {
-    const VirusSpecies = sequelize.define("VirusSpecies", {
-        
-    });
+    VirusSpecies.associate = models => {
+        VirusSpecies.belongsTo(models.Species, { foreignKey: 'id' });
+    };
 
     return VirusSpecies;
-}
+};
