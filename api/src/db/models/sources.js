f66f6708fe2d3af821069b75c1702c1c03c759a5
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:30:35 +0100

    Set up sources model

--- api/src/db/models/sources.js
+++ api/src/db/models/sources.js
@@ -1,10 +1,23 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
-
-export default (sequelize) => {
-    const Sources = sequelize.define("Sources", {
-        
-    });
+export default sequelize => {
+    const Sources = sequelize.define(
+        'Sources',
+        {
+            id: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'id',
+            },
+            sourceName: { type: Sequelize.STRING(100), field: 'source_name' },
+        },
+        {
+            tableName: 'sources',
+            timestamps: true,
+            createdOn: 'created_on',
+            updatedOn: 'updated_on',
+        },
+    );
 
     return Sources;
-}
+};
