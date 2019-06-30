9c56d32fc7be95c5cfa73b684f27438f6a7c51f5
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:40:52 +0100

    Set up user roles model

--- api/src/db/models/user-roles.js
+++ api/src/db/models/user-roles.js
@@ -1,10 +1,34 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
+export default sequelize => {
+    const UserRoles = sequelize.define(
+        'UserRoles',
+        {
+            userId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'user_id',
+            },
+            roleId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'role_id',
+            },
+        },
+        {
+            tableName: 'user_roles',
+            timestamps: true,
+            createdOn: 'created_on',
+            updatedOn: 'updated_on',
+        },
+    );
 
-export default (sequelize) => {
-    const UserRoles = sequelize.define("UserRoles", {
-        
-    });
+    UserRoles.associate = models => {
+        UserRoles.belongsTo(models.Roles, {
+            sourceKey: 'roleId',
+            foreignKey: 'id',
+        });
+    };
 
     return UserRoles;
-}
+};
