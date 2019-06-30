33ace09c9fe23c07b6d1229bdb627babee0b0c40
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:30:59 +0100

    Set up types model

--- api/src/db/models/types.js
+++ api/src/db/models/types.js
@@ -1,10 +1,24 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
-
-export default (sequelize) => {
-    const Types = sequelize.define("Types", {
-        
-    });
+export default sequelize => {
+    const Types = sequelize.define(
+        'Types',
+        {
+            id: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'id',
+            },
+            disease: { type: Sequelize.STRING(100), field: 'disease' },
+            serotype: { type: Sequelize.STRING(100), field: 'serotype' },
+        },
+        {
+            tableName: 'types',
+            timestamps: true,
+            createdOn: 'created_on',
+            updatedOn: 'updated_on',
+        },
+    );
 
     return Types;
-}
+};
