04f9ff6c0b4d220c245038b3fdf151679306c4a1
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:29:53 +0100

    Set up virus types model

--- api/src/db/models/virus_type.js
+++ api/src/db/models/virus_type.js
@@ -1,10 +1,29 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
+export default sequelize => {
+    const VirusType = sequelize.define(
+        'VirusType',
+        {
+            virusId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'virus_id',
+            },
+            typeId: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'type_id',
+            },
+        },
+        {
+            tableName: 'virus_type',
+            timestamps: false,
+        },
+    );
 
-export default (sequelize) => {
-    const VirusType = sequelize.define("VirusType", {
-        
-    });
+    VirusType.associate = models => {
+        VirusType.belongsTo(models.Types, { foreignKey: 'id' });
+    };
 
     return VirusType;
-}
+};
