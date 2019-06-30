ea8c283841212f807cc34bbf2b151d32b0ffbb9c
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:41:02 +0100

    Set up user model

--- api/src/db/models/users.js
+++ api/src/db/models/users.js
@@ -1,10 +1,34 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
+export default sequelize => {
+    const Users = sequelize.define(
+        'Users',
+        {
+            id: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'id',
+            },
+            firstName: { type: Sequelize.STRING(50), field: 'first_name' },
+            lastName: { type: Sequelize.STRING(50), field: 'last_name' },
+            password: { type: Sequelize.STRING(50), field: 'password' },
+            email: { type: Sequelize.STRING(355), field: 'email' },
+            role: { type: Sequelize.STRING(50), field: 'role' },
+        },
+        {
+            tableName: 'users',
+            timestamps: true,
+            createdOn: 'created_on',
+            updatedOn: 'updated_on',
+        },
+    );
 
-export default (sequelize) => {
-    const Users = sequelize.define("Users", {
-        
-    });
+    Users.associate = models => {
+        Users.hasMany(models.UserRole, {
+            sourceKey: 'id',
+            foreignKey: 'userId',
+        });
+    };
 
     return Users;
-}
+};
