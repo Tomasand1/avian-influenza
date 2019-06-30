1601508eeed51bb3056de66f5d91cc1bdebc710c
Tomas <andriu.tomas@gmail.com>
2019-06-30 21:30:45 +0100

    Set up species model

--- api/src/db/models/species.js
+++ api/src/db/models/species.js
@@ -1,10 +1,24 @@
-import * as sequelize from "sequelize";
+import * as sequelize from 'sequelize';
 
-
-export default (sequelize) => {
-    const Species = sequelize.define("Species", {
-        
-    });
+export default sequelize => {
+    const Species = sequelize.define(
+        'Species',
+        {
+            id: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                field: 'id',
+            },
+            speciesType: { type: Sequelize.STRING(100), field: 'species_type' },
+            SpeciesName: { type: Sequelize.STRING(100), field: 'species_name' },
+        },
+        {
+            tableName: 'species',
+            timestamps: true,
+            createdOn: 'created_on',
+            updatedOn: 'updated_on',
+        },
+    );
 
     return Species;
-}
+};
