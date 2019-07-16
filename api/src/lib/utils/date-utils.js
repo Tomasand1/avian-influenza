ce51415996f7c58835119e40a2f40afdd140a12a
Tomas <andriu.tomas@gmail.com>
2019-07-16 18:01:33 +0300

    Add date validation function

--- api/src/lib/utils/date-utils.js
+++ api/src/lib/utils/date-utils.js
@@ -1,16 +1,22 @@
 import moment from 'moment';
 
-// TODO: create date validation
 export class DateUtils {
     static getFormatedDate(date) {
-        if (date !== null) {
-            let formatedDate = moment(date, 'DD/MM/YYYY', true);
-            if (formatedDate.isValid()) {
-                return formatedDate;
-            }
-            throw Error('Date provided is not valid');
-        }
+        const formatedDate = moment(date, 'DD/MM/YYYY', true);
 
-        return null;
+        const validatedDate = validatedDate(formatedDate);
+
+        if (!validatedDate) throw Error('Date provided is not valid');
+
+        return validatedDate;
+    }
+
+    static validateDate(date) {
+        if (date !== null) {
+            if (date.isValid()) {
+                return date;
+            }
+        }
+        return false;
     }
 }
